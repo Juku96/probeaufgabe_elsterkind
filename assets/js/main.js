ebbc177228da864ffd7015b75612dcdf9dc08649
Juku96 <juliankuhrt@gmail.com>
2022-06-10 13:51:41 +0200

    Aufgabe 6

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,13 @@
 let selectbox;
 let buttons;
+let krankenkassenList;
 
 window.onload = function () {
     selectbox = document.querySelector('#select-input select');
+    const btnGroup = document.querySelector('.btn-group');
     buttons = document.querySelectorAll('.btn-group button');
+
+    krankenkassenList = document.querySelector('.krankenkassen-liste');
 
     // Buttons mit Krankenkassen füllen & Event listener hinzufügen
     for( let i=0; i< buttons.length; i++) {
@@ -18,18 +22,37 @@ window.onload = function () {
                 return;
             }
             selectbox.value = this.name;
+            btnGroup.classList.remove('active');
             onChangeKrankenkasse();
-        })
+        });
     }
 
     // Selectbox mit Inhalten füllen
     for( let i=0; i < Object.keys(krankenkassen).length; i++) {
-        if(i==0) {
-            selectbox.innerHTML += "<option>"+ krankenkassen[i].name +"</option>";
-        } else {
-            selectbox.innerHTML += "<option>"+ krankenkassen[i].name +"</option>";
-        }
+        // Ich wollte erst ein Selected bei dem ersten Element einfügen und hab vergessen dann die if-Verzeigung zu entfernen 
+
+        krankenkassenList.innerHTML += "<li class='krankenkassen-item' data-name='"+krankenkassen[i].name+"'>"+krankenkassen[i].name+"</li>";
+        selectbox.innerHTML += "<option>"+ krankenkassen[i].name +"</option>";
     }
+
+    let krankenkassenItems = document.querySelectorAll('.krankenkassen-item');
+
+    for( let i=0; i< krankenkassenItems.length; i++) {
+        krankenkassenItems[i].addEventListener("click", function(e) {
+            selectbox.value = this.dataset.name;
+            onChangeKrankenkasse();
+            krankenkassenList.style.display = 'none';
+        })
+    }
+
+    selectbox.addEventListener("focus", function() {
+        btnGroup.classList.toggle('active');
+    });
+
+    selectbox.addEventListener("focusout", function() {
+        btnGroup.classList.remove('active');
+    });
+
 }
 
 // Wird beim ändern der SelectBox aufgerufen
@@ -50,4 +73,8 @@ function onChangeKrankenkasse() {
     outputPercentage.innerHTML = refund.refund_percentage * 100 + "%";
     outputPrice.innerHTML = refund.payment + "€";
     circle.style.strokeDashoffset = 628 - 628 * refund.refund_percentage;
+}
+
+function showKrankenkassenList() {
+    krankenkassenList.style.display = 'block';
 }
